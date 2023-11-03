950a626a20b54a69ac2725bab17cbe6d8817d762
JYJSXX <95729892+JYJSXX@users.noreply.github.com>
2023-11-03 21:20:10 +0800

    1

--- simulator/sim/build.cpp
+++ simulator/sim/build.cpp
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <iostream>
 #include <assert.h>
+#include <fstream>
 #include "include/debug.h"
 
 extern uint8_t pmem[];
@@ -9,5 +10,12 @@ extern uint8_t pmem[];
 uint64_t load_img(char *img_file){
 
 // Lab2 TODO: load the 'img_file' to the start of pmem, and return its size
-
+    std::string filename = std::string(img_file);
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+    assert(file.is_open());
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+    file.read((char*)pmem, size);
+    file.close();
+    return size;
 }
